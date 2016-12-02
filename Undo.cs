0af28eb138583049302d112cbb55667d6b7476c6
Dylan Rodriquez <dylanmr@uchicago.edu>
2016-12-02 13:17:59 -0600

    for an undo button in a Unity Engine Game

---
+++ Undo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Undo : MonoBehaviour {
+
+	Stack undoStack = new Stack();
+	Stack redoStack = new Stack();
+
+	public void setUndoScore ()
+	{
+		var fix = undoStack.Peek ();
+		var scoreKeeper = gameObject.GetComponent<InputField>();
+		scoreKeeper.text = fix;
+		redoStack.Push (undoStack.Pop);
+	}
+
+
+	public void setRedoScore ()
+	{
+		var unfix = redoStack.Peek ();
+		var unfixableScoreKeeper = gameObject.GetComponent<InputField>();
+		unfixableScoreKeeper.text = unfix;
+		undoStack.Push (redoStack.Pop);
+	}
+ }
+
